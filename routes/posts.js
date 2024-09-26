02cf7b87a084b6ebe3d0c527f0c27644ddd751a9
Nancy Hunter <hunternancym@gmail.com>
2024-09-25 20:06:49 -0400

    rearrange file structure for admin

--- routes/posts.js
+++ routes/posts.js
@@ -7,12 +7,4 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //Post Routes - simplified for now
 router.get("/:id", postsController.getPost);
 
-router.post("/createPost", upload.single("file"), postsController.createPost);
-
-router.put("/onSale/:id", postsController.onSale);
-
-router.put("/soldOut/:id", postsController.soldOut);
-
-router.delete("/deletePost/:id", postsController.deletePost);
-
 module.exports = router;
