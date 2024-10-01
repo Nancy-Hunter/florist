7da115bf9f3e28576ff7fa6ba8cd8533d443bf4d
Nancy Hunter <hunternancym@gmail.com>
2024-10-01 11:27:35 -0400

    delete from cart updates total sum and count

--- public/main.js
+++ public/main.js
@@ -89,13 +89,13 @@ function updateCart() {
     tr.appendChild(qty_td);
     
     let editButton = document.createElement("td")
-    editButton.innerHTML = `<span data-title="${item.productTitle}" class ="editCart"><svg xmlns="http://www.w3.org/2000/svg"     width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+    editButton.innerHTML = `<span data-id="${id}" class ="editCart"><svg xmlns="http://www.w3.org/2000/svg"     width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
     </svg> Edit</span>`
     tr.appendChild(editButton)
     
     let deleteButton = document.createElement("td")
-    deleteButton.innerHTML = `<span data-title="${item.productTitle}" class ="deleteCart"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+    deleteButton.innerHTML = `<span data-id="${id}" class ="deleteCart"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
     </svg> Delete</span>`
@@ -114,18 +114,20 @@ deleteFromCartButton.forEach(function (el) {
   el.addEventListener('click', deleteFromCart)
 })
 function deleteFromCart(event) {
-  console.log('clicked delete button', event.target.dataset.title)
-  let cart = JSON.parse(localStorage.getItem("cart")); 
-  let productTitle = event.target.dataset.title
-  console.log(cart)
+  let cart = JSON.parse(localStorage.getItem("cart"))
+  let productID = event.target.dataset.id
   for (let id in cart) {
     let item = cart[id]
-    console.log(cart[id])
-    console.log(item)
-    if (item.productTitle == productTitle) {
-        console.log('found match')
+    if (id == productID) {    
+      sum -= item.qty * item.price
+      count -= item.qty
+      delete cart[id]
     }
-}
+  }
+  localStorage.setItem("sum", sum)
+  localStorage.setItem("count", count)
+  localStorage.setItem("cart", JSON.stringify(cart))
+  window.location.reload()
 }
 
 //Adds edit buttons in checkout page
