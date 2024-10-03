4072d0963e66a23cef2ec0741f4cc921fda8dcb3
Jordan Bryant-Smith <134249845+Gazerbeam2@users.noreply.github.com>
2024-10-02 23:12:10 -0500

    removed links

--- public/main.js
+++ public/main.js
@@ -96,31 +96,3 @@ function updateCart() {
   }
 }
 updateCart()
-
-const sympathy_image_link = document.querySelector(".sympathy_image_class")
-const Best_Seller_Link = document.querySelector(".Best_Seller_image_class")
-const anniversary_image_link = document.querySelector(".anniversary_image_class")
-const birthday_image_link = document.querySelector(".birthday_image_class")
-const congratulations_link = document.querySelector(".congratulations_images_class")
-
-
-
-sympathy_image_link.addEventListener('click', () =>{
-  window.location.href = '/flowersFor/sympathy'
-})
-
-Best_Seller_Link.addEventListener('click',() =>{
-  window.location.href = '/flowersFor/best%20Sellers'
-})
-
-anniversary_image_link.addEventListener('click',() =>{
-  window.location.href = '/flowersFor/anniversary'
-})
-
-birthday_image_link.addEventListener('click',() =>{
-  window.location.href = '/flowersFor/birthday'
-})
-
-congratulations_link.addEventListener('click',() =>{
-  window.location.href ='/flowersFor/congratulations'
-})
