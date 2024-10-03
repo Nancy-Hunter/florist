37985c84f69e6a049e72086822aee370f486474a
Nancy Hunter <hunternancym@gmail.com>
2024-10-02 22:10:26 -0400

    added add to cart animation and made sure icons in cart also respond to eventlistener

--- public/main.js
+++ public/main.js
@@ -59,6 +59,12 @@ if (id in cart) {
   localStorage.setItem("cart", JSON.stringify(cart));
   localStorage.setItem("sum", sum)
   localStorage.setItem("count", count)
+  let shoppingCart = document.querySelector('.shopping-cart')
+  shoppingCart.classList.add('active');
+  shoppingCart.setAttribute('data-product-count', count)
+  setTimeout(() => {
+    shoppingCart.classList.remove('active');
+}, 1000)
 }
 
 //Updates table in checkout
@@ -89,16 +95,11 @@ function updateCart() {
     tr.appendChild(qty_td);
     
     let editButton = document.createElement("td")
-    editButton.innerHTML = `<span data-id="${id}" class ="editCart"><svg xmlns="http://www.w3.org/2000/svg"     width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
-    </svg> Edit</span>`
+    editButton.innerHTML = `<span data-id="${id}" class ="editCart"><i data-id="${id}" class = "editCart fas fa-pencil-alt px-2"></i>Edit</span>`
     tr.appendChild(editButton)
     
     let deleteButton = document.createElement("td")
-    deleteButton.innerHTML = `<span data-id="${id}" class ="deleteCart"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-    </svg> Delete</span>`
+    deleteButton.innerHTML = `<span data-id="${id}" class ="deleteCart"><i data-id="${id}" class="deleteCart fa fa-times px-2 "></i>Delete</span>`
     tr.appendChild(deleteButton)
     
     
